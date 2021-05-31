1eaac5a7af5a32054c67d93a5100079ccf9a73d3
Mohammad Alisafaee <mohammad.alisafaee@epfl.ch>
2021-06-01 01:40:51 +0200

    change default env

--- acceptance-tests/src/test/scala/ch/renku/acceptancetests/tooling/TestsDefaults.scala
+++ acceptance-tests/src/test/scala/ch/renku/acceptancetests/tooling/TestsDefaults.scala
@@ -44,6 +44,6 @@ object TestsDefaults {
         .fold(error => throw new Exception(error.prettyPrint()), identity)
     else
       TestsDefaults(
-        env = Some("https://dev.renku.ch")
+        env = Some("https://mohammad.dev.renku.ch") // TODO restore default
       )
 }
